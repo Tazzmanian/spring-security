695d9ce952a1c1dbaa9f267392cad59b21ef45e9
Tazzmanian <banskaliqtabg@yahoo.com>
2025-09-18 22:39:31 +0300

    Set Authentication Provider to use the user details service

--- src/main/java/com/spring/secutiry/SecurityDemo/config/SecurityProdConfig.java
+++ src/main/java/com/spring/secutiry/SecurityDemo/config/SecurityProdConfig.java
@@ -11,9 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
-@Profile("!prod")
+@Profile("prod")
 @Configuration
-public class SecurityConfig {
+public class SecurityProdConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
